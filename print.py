aa75c9dda2814b0194f41625ed28978f9c967eb4
anshul gupta <anshulgupta3498@gmail.com>
2018-06-06 22:41:37 +0530

    first change

---
+++ print.py
@@ -0,0 +1,38 @@
+#Ques1. To print anything you want
+print("we are learning python program\n\n")
+print(10*"*")
+
+#Ques2. To join acad+view
+x="acad"
+y="view"
+print(x+y,"\n\n")
+print(10*"*")
+
+#Ques3. To take the input of 3 variables x, y and z . Print their values on screen.
+x=input("enter the value of x:")
+y=input("enter value of y:")
+z=input("enter value of z:")
+print("x=",x)
+print("y=",y)
+print("z=",z,"\n\n")
+print(10*"*")
+
+#Ques4. To print “Let’s get started” on screen.
+print(''' "Let's get started"\n\n''')
+print(10*"*")
+
+#Ques5. To print the following values using placeholders.
+#    s=”Acadview”
+#    course=”Python”
+#    fees=5000
+s=''' "Acadview" '''
+course=''' "Python" '''
+fees="5000"
+print("s=%s\ncourse=%s\nfees=%s\n\n"%(s,course,fees))
+print(10*"*")
+
+#Ques6. To do some exercise
+name='''”Tony Stark”'''
+salary=1000000
+print('%s' '%d\n\n'%(name,salary))
+print(10*"*")
